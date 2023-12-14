9a2edc3be2bb68d7763bde4d662e6ec099dc2b7e
unknown <mikognrl@gmail.com>
2023-12-14 15:13:13 +0800

    Added controller folder and routing

--- public/index.php
+++ public/index.php
@@ -4,11 +4,11 @@
       
       // Instantiate the router object
        $router = new Router();
-
+     
       // Register routes
-      $router->get('/', 'controllers/home.php');
-      $router->get('/listings', 'controllers/listings/index.php');
-      $router->get('/listings/create', 'controllers/listings/create.php');
+       $router->get('/', 'controllers/home.php');
+       $router->get('/listings', 'controllers/listings/index.php');
+       $router->get('/listings/create', 'controllers/listings/create.php');
      
 
      //  pass the current uri and request method into the $router object 
